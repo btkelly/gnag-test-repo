fa21f489a58defba0ded027ada17cd3518bee609
Bryan Kelly <bkelly@detroitlabs.com>
2017-02-09 17:46:30 -0500

    Removed support library for example

--- app/src/main/java/io/btkelly/gnagtest/MainActivity.java
+++ app/src/main/java/io/btkelly/gnagtest/MainActivity.java
@@ -1,10 +1,11 @@
 package io.btkelly.gnagtest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
