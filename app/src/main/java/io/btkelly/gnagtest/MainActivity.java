7b979254e8f245231330b4e748e427dfcce60272
Bryan Kelly <bkelly@detroitlabs.com>
2017-02-09 17:12:42 -0500

    Added a few violations to MainActivity

--- app/src/main/java/io/btkelly/gnagtest/MainActivity.java
+++ app/src/main/java/io/btkelly/gnagtest/MainActivity.java
@@ -2,6 +2,7 @@ package io.btkelly.gnagtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            Log.e("TEST", "Should fail Gnag Check");
+        } catch (Exception e) {
+
+        }
     }
 }
